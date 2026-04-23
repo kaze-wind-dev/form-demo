9c8a48040950ff71adabd59b1a13e703e4761fdc
HayateIwamoto <hayate.iwamoto@trq.co.jp>
2026-04-23 14:19:15 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 const site = process.env.SITE_URL || 'http://localhost:4321';
 
 // https://astro.build/config
 export default defineConfig({
   site,
+  adapter: cloudflare(),
   integrations: [sitemap(), react()],
 });
